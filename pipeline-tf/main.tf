38df38ed7056bf685fb4734079737bd0ddf1c741
diunkz <noahdiunkz@gmail.com>
2025-10-14 19:31:06 -0400

    finish: main.tf

--- pipeline-tf/main.tf
+++ pipeline-tf/main.tf
@@ -10,12 +10,8 @@ data "aws_caller_identity" "current" {}
 locals {
   # ARN do Layer Público AWSSDKPandas (Python 3.13)
   pandas_layer_arn_dynamic = "arn:aws:lambda:us-east-1:336392948345:layer:AWSSDKPandas-Python313:4" 
-  # pandas_layer_arn_dynamic = "arn:aws:lambda:us-east-1:336392948345:layer:AWSSDKPandas-Python39:31"
-  # pandas_layer_arn_dynamic = "arn:aws:lambda:us-east-1:770693421928:layer:Klayers-p312-pandas:17"
-  # Constrói o ARN da LabRole dinamicamente
   lab_execution_role_arn_dynamic = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/${var.lab_execution_role_name}"
 }
-
 
 # ----------------------------------------------------------------------
 # 1. ARMAZENAMENTO (S3 DATA LAKE)
@@ -49,15 +45,6 @@ resource "aws_s3_object" "lambda_collector_code_upload" {
   source = "../lambda_package.zip" 
   etag   = filemd5("../lambda_package.zip")
 }
-
-# 2.3. Upload do Código Lambda TRIGGER (O orquestrador do Glue)
-resource "aws_s3_object" "lambda_trigger_code_upload" {
-  bucket = aws_s3_bucket.data_lake_bucket.id
-  key    = "lambda-code/glue-trigger-code.zip"
-  source = "../glue_trigger_package.zip" # Este arquivo deve estar na pasta atual (pipeline-tf/)
-  etag   = filemd5("../glue_trigger_package.zip")
-}
-
 
 # ----------------------------------------------------------------------
 # 3. AWS LAMBDA FUNCTION (SCRAPING - Coleta Inicial)
@@ -111,58 +98,8 @@ resource "aws_lambda_permission" "allow_cloudwatch" {
   source_arn    = aws_cloudwatch_event_rule.daily_schedule.arn
 }
 
-# # ----------------------------------------------------------------------
-# # 5. NOVA FUNÇÃO LAMBDA (TRIGGER GLUE - Requisito 4)
-# # ----------------------------------------------------------------------
-
-# resource "aws_lambda_function" "glue_trigger_lambda" {
-#   function_name    = "${var.project_name}-glue-trigger" 
-#   role             = local.lab_execution_role_arn_dynamic 
-#   handler          = "glue_trigger.lambda_handler"
-#   runtime          = "python3.13"
-#   timeout          = 300 
-
-#   # Aponta para o ZIP que está no S3 (Staging)
-#   s3_bucket        = aws_s3_object.lambda_trigger_code_upload.bucket
-#   s3_key           = aws_s3_object.lambda_trigger_code_upload.key
-  
-#   environment {
-#     variables = {
-#       GLUE_JOB_NAME = aws_glue_job.ibov_etl_job.name
-#     }
-#   }
-#   depends_on = [aws_s3_object.lambda_trigger_code_upload]
-# }
-
-# # ----------------------------------------------------------------------
-# # 6. GATILHO S3 (S3 -> LAMBDA) - Requisito 3
-# # ----------------------------------------------------------------------
-
-# resource "aws_lambda_permission" "allow_s3_to_invoke_glue_trigger" {
-#   statement_id  = "AllowExecutionFromS3"
-#   action        = "lambda:InvokeFunction"
-#   function_name = aws_lambda_function.glue_trigger_lambda.function_name
-#   principal     = "s3.amazonaws.com"
-#   source_arn    = aws_s3_bucket.data_lake_bucket.arn 
-#   source_account = data.aws_caller_identity.current.account_id # <<< Adiciona o ID da Conta
-# }
-
-# resource "aws_s3_bucket_notification" "trigger_glue_etl" {
-#   bucket = aws_s3_bucket.data_lake_bucket.id
-
-#   lambda_function {
-#     lambda_function_arn = aws_lambda_function.glue_trigger_lambda.arn
-#     events              = ["s3:ObjectCreated:*"] 
-    
-#     filter_prefix       = "raw/ibov/" 
-#     filter_suffix       = ".parquet" 
-#   }
-  
-#   depends_on = [aws_lambda_permission.allow_s3_to_invoke_glue_trigger]
-# }
-
 # ----------------------------------------------------------------------
-# 7. GLUE ETL JOB (Requisito 5 e 6)
+# 5. GLUE ETL JOB (Requisito 5 e 6)
 # ----------------------------------------------------------------------
 
 resource "aws_glue_job" "ibov_etl_job" {
@@ -214,10 +151,9 @@ resource "aws_athena_workgroup" "primary_workgroup" {
   # Configuração da saída de consultas
   configuration {
     result_configuration {
-      # 💥 CRÍTICO: Define o local de saída da consulta 💥
       output_location = "s3://${aws_s3_bucket.data_lake_bucket.id}/athena-results/"
       
-      # Opcional, mas recomendado: Criptografia dos resultados
+      # Criptografia dos resultados
       /*
       encryption_configuration {
         encryption_option = "SSE_S3" 
